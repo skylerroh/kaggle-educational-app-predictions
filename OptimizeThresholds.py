6a2e0e0416a60be2ca0fb389ce93aedd5042f01a
Skyler Roh <skylerroh@gmail.com>
2020-04-05 19:41:09 +0000

    linear programming, optimize thresholds for qwk metric

--- OptimizeThresholds.py
+++ OptimizeThresholds.py
@@ -10,13 +10,19 @@ class OptimizedRounder(object):
         self.labels = labels
     
     def _kappa_loss(self, coef, X, y):
+#         print(coef)
+        if len(set(coef)) != len(coef):
+            return 0
         preds = pd.cut(X, [-np.inf] + list(np.sort(coef)) + [np.inf], labels = self.labels)
         return -cohen_kappa_score(y, preds, weights = 'quadratic')
+    
     
     def fit(self, X, y):
         loss_partial = partial(self._kappa_loss, X = X, y = y)
         initial_coef = [0.5, 1.5, 2.5]
-        self.coef_ = sp.optimize.minimize(loss_partial, initial_coef, method = 'nelder-mead')
+        constraints = ({'type': 'ineq', 'fun' : lambda x: x[1] - x[0] - 0.001},
+                       {'type': 'ineq', 'fun' : lambda x: x[2] - x[1] - 0.001})
+        self.coef_ = sp.optimize.minimize(loss_partial, initial_coef, method = 'COBYLA', constraints=constraints)
     
     def predict(self, X, coef=None):
         coef = coef if coef else self.coefficients()
